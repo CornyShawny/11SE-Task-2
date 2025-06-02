944c71668af0370997239fdba22f1375bfb40cb6
CornyShawny <160194015+CornyShawny@users.noreply.github.com>
2025-06-02 11:09:07 +1000

    small change
    formatting + one comment

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ root = tb.Window(themename="cyborg") # theme for tkinter
 root.title("Typing Speed Test") # window title
 root.geometry("1920x1080") # window size
 
+# title within the window
 tb.Label(
     root,
     text = "Typing Speed Test",
-    font = ("System", 48, "bold",),
+    font = ("System", 48, "bold"),
     bootstyle = "light"
-    ).pack(pady=10)
+).pack(pady=10)
 
 root.mainloop()
