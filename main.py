8ad29d0ac50b09ff8fbdf9b3cea4fcea836d5855
CornyShawny <160194015+CornyShawny@users.noreply.github.com>
2025-06-03 15:10:28 +1000

    adding to code
    I added subheading and a dropdown box and a submit button. These changes all worked. Now build and testing is donw so im up to review.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import ttkbootstrap as tb # theme for tkinter
 
 root = tb.Window(themename="cyborg") # theme for tkinter
@@ -6,11 +7,49 @@ root.title("Typing Speed Test") # window title
 root.geometry("1920x1080") # window size
 
 # title within the window
-tb.Label(
+title = tb.Label(
     root,
     text = "Typing Speed Test",
     font = ("System", 48, "bold"),
     bootstyle = "light"
-).pack(pady=10)
+)
+title.pack(pady=10)
+
+# subheading welcoming the user to the app
+subheading = tb.Label(
+    root,
+    text = "Welcome to the Typing Speed Test!!!",
+    font = ("System", 32),
+    bootstyle = "light"
+)
+subheading.pack(pady=10)
+
+# subheading asking how many words for the test
+howmanywords = tb.Label(
+    root,
+    text = "How many words?",
+    font = ("System", 24),
+    bootstyle = "light"
+)
+howmanywords.pack(pady=5)
+
+# choices for word count
+wordcountchoices = ["10", "25", "50", "100"]
+
+# dropdown box to select word count
+wordcount = ttk.Combobox(root, values = wordcountchoices)
+wordcount.set("Select an option")
+wordcount.pack(pady=10)
+
+'''
+function for selecting word count and outputting the choice into the console
+'''
+def on_select():
+    selected_option = wordcount.get()
+    print(f"You selected: {selected_option}")
+
+# submit button to submit chosen word count
+submit = tb.Button(root, text="Submit", command=on_select)
+submit.pack(pady=10)
 
 root.mainloop()
